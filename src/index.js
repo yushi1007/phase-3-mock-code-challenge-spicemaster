c9f4b2b9c4deb5248d4e2f2ef8e290648ac54495
Yu Shi <yushiys95@gmail.com>
2021-03-24 21:20:37 -0400

    second try

--- src/index.js
+++ src/index.js
@@ -1,106 +1,221 @@
 // write your code here
+const updateForm = document.querySelector('form#update-form')
+const ingredientFrom = document.querySelector('form#ingredient-form')
+const spiceImages = document.querySelector('div#spice-images')
+// 1
 
-function allSpiceBlends() {
-    fetch('http://localhost:3000/spiceblends')
-        .then(response => response.json())
-        .then(spiceBlends => {
-                spiceBlends.forEach(spiceBlend => {
-                    spiceBlendImage(spiceBlend)
-            })
-        })
-}
-allSpiceBlends()
 
-function spiceBlendImage(spiceBlendInfo) {
-    const outerDiv = document.createElement('div')
-    outerDiv.classList.add('image')
-    outerDiv.dataset.id = spiceBlendInfo.id
+function displaySpiceBlend(spiceDetails) {
 
-    outerDiv.innerHTML = `<img class="detail-image" src="${spiceBlendInfo.image}" alt="${spiceBlendInfo.title}" />`
+    const spiceBlendDetail = document.querySelector('div#spice-blend-detail')
 
-    const collection = document.querySelector('div#spice-images')
-    collection.append(outerDiv)
+    const img = spiceBlendDetail.querySelector('img')
+    img.src = spiceDetails.image
+    img.alt = spiceDetails.title
 
-}
+    const h2 = document.querySelector('h2.title')
+    h2.textContent = spiceDetails.title
 
-function spiceBlendLoad(firstSpiceBlend) {
-        const spiceBlendDetail = document.querySelector('div#spice-blend-detail')
-        spiceBlendDetail.dataset.id = firstSpiceBlend.id
-        
-        spiceBlendDetail.innerHTML = `
-        <img class="detail-image" src="${firstSpiceBlend.image}" alt="${firstSpiceBlend.title}" />
-        <h2 class="title">${firstSpiceBlend.title}</h2>`
+    updateForm.dataset.id = spiceDetails.id
+    ingredientFrom.dataset.id = spiceDetails.id
 
-        
+    const ul = document.querySelector('ul.ingredients-list')
+    ul.innerHTML = ''
+    spiceDetails.ingredients.forEach(spiceIngredients => {
+            const li = document.createElement('li')
+            
+            li.dataset.id = spiceIngredients.id
+            li.textContent = spiceIngredients.name
+
+            ul.append(li)
+    })
 
 }
 
-function firstSpiceBlend() {
-    fetch('http://localhost:3000/spiceblends')
+
+fetch('http://localhost:3000/spiceblends/1')
     .then(response => response.json())
-    .then(SpiceBlend => {
-        
-        spiceBlendLoad(SpiceBlend[0])
+    .then(spiceObject => {
+                displaySpiceBlend(spiceObject)
+    })
+
+// 2
+
+updateForm.addEventListener('submit', e=> {
+    e.preventDefault()
+
+    const title = e.target.title.value
+
+    fetch(`http://localhost:3000/spiceblends/${e.target.dataset.id}`, {
+        method: 'PATCH',
+        headers:{       
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({title: title})
+    })
+        .then(response => response.json())
+        .then(newTitle => {
+            const h2 = document.querySelector('h2.title')
+            h2.textContent = newTitle.title
         })
-    }
-firstSpiceBlend()
-
-const spiceImage = document.querySelector('div#spice-images')
-
-spiceImage.addEventListener('click', e => {
-        if (e.target.matches('img')){
-                const spice = e.target.closest('div.image')
-
-                fetch(`http://localhost:3000/spiceblends/${spice.dataset.id}`)
-                .then(response => response.json())
-                .then(spiceInfo => {
-                    spiceBlendLoad(spiceInfo)
-                })
-        }
 })
 
-const form = document.querySelector('form#update-form')
+// 3 and 4
+ingredientFrom.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const name = e.target['ingredient-name'].value
+        const li = document.createElement('li')
+        li.textContent = name
+
+        const ul = document.querySelector('ul.ingredients-list')
+        ul.append(li)
+
+        ingredientFrom.reset()
+
+        fetch('http://localhost:3000/ingredients', {
+            method: 'POST',
+            headers:{       
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name, 
+                spiceblendId: parseInt(e.target.dataset.id)
+            })
+        })
+            .then(response => response.json())
+            .then(newIngredient => console.log(newIngredient))
+        
+})
+
+// 5
+fetch('http://localhost:3000/spiceblends')
+        .then(response => response.json())
+        .then(spiceImage => {
+            
+        const spiceImages = document.querySelector('div#spice-images')
+
+                spiceImage.forEach(spiceBlendImage => {
+
+                    const img = document.createElement('img')
+
+                    img.dataset.id = spiceBlendImage.id
+                    img.src = spiceBlendImage.image
+                    img.alt = spiceBlendImage.title
+
+                    spiceImages.append(img)
+
+        })
+    })
+
+
+// 6
+spiceImages.addEventListener('click', e => {
+
+    if (e.target.matches('img')) {
+
+            const spiceId = e.target.dataset.id
+        
+        fetch(`http://localhost:3000/spiceblends/${spiceId}`)
+            .then(response => response.json())
+            .then(spiceInfo => {
+                    displaySpiceBlend(spiceInfo)
+        })
+    }
+})
+
+
+
+// FIRST TRY
+
+// function allSpiceBlends() {
+//     fetch('http://localhost:3000/spiceblends')
+//         .then(response => response.json())
+//         .then(spiceBlends => {
+//                 spiceBlends.forEach(spiceBlend => {
+//                     spiceBlendImage(spiceBlend)
+//             })
+//         })
+// }
+// allSpiceBlends()
+
+// function spiceBlendImage(spiceBlendInfo) {
+//     const outerDiv = document.createElement('div')
+//     outerDiv.classList.add('image')
+//     outerDiv.dataset.id = spiceBlendInfo.id
+
+//     outerDiv.innerHTML = `<img class="detail-image" src="${spiceBlendInfo.image}" alt="${spiceBlendInfo.title}" />`
+
+//     const collection = document.querySelector('div#spice-images')
+//     collection.append(outerDiv)
+
+// }
+
+// function spiceBlendLoad(firstSpiceBlend) {
+//         const spiceBlendDetail = document.querySelector('div#spice-blend-detail')
+//         const spiceTitle = document.querySelector('#update-form')
+//         spiceBlendDetail.dataset.id = firstSpiceBlend.id
+        
+//         spiceBlendDetail.innerHTML = `
+//         <img class="detail-image" src="${firstSpiceBlend.image}" alt="${firstSpiceBlend.title}" />
+//         <h2 class="title">${firstSpiceBlend.title}</h2>`
+
+//         spiceTitle.dataset.id = firstSpiceBlend.id
+//         spiceTitle.title.value = firstSpiceBlend.title
+
+// }
+
+// function firstSpiceBlend() {
+//     fetch('http://localhost:3000/spiceblends')
+//     .then(response => response.json())
+//     .then(SpiceBlend => {
+        
+//         spiceBlendLoad(SpiceBlend[0])
+//         })
+//     }
+    
+// firstSpiceBlend()
+
+// const spiceImage = document.querySelector('div#spice-images')
+
+// spiceImage.addEventListener('click', e => {
+//         if (e.target.matches('img')){
+//                 const spice = e.target.closest('div.image')
+
+//                 fetch(`http://localhost:3000/spiceblends/${spice.dataset.id}`)
+//                 .then(response => response.json())
+//                 .then(spiceInfo => {
+//                     spiceBlendLoad(spiceInfo)
+//                 })
+//         }
+// })
+
+// const form = document.querySelector('form#update-form')
 
 // form.addEventListener('submit', e=> {
 //         e.preventDefault()
 
 //         const id = e.target.dataset.id
 //         const title = e.target.title.value
-        
-//         const newTitle = {
-//             title
-//         }
+
 //         fetch(`http://localhost:3000/spiceblends/${id}`, {
 //             method: 'PATCH',
 //             headers:{
-//                 'Content-Type': 'application/json',
-//                 'Accept': 'application/json'
+//                 'Content-Type': 'application/json'
 //               },
-//               body: JSON.stringify(newTitle)
+//               body: JSON.stringify({title})
 //         })
 //         .then(response => response.json())
-//         .then(title => (title))
+//         .then(titleObject => {
+//             const h2 = document.querySelector('h2.title')
+//             h2.textContent = titleObject.title
+//         })
 
 // })
 
-// function spiceIngredients() {
 
-//     fetch('http://localhost:3000/ingredients')
-//         .then(response => response.json())
-//         .then(ingredients => {
-//             ingredients.forEach(ingredient => {
-//                 const li = document.createElement('li')
-
-//                 li.dataset.id = ingredient.id
-
-//                 li.innerHTML = `<a>${ingredient.name}</a>`
-
-//                 const ul = document.querySelector('ul.ingredients-list')
-
-//                 ul.append(li)
-//             })
-//         })
-// }
 // spiceIngredients()
 // function ingredientsInfo(ingredients) {
 //     const ingredientContainer = document.querySelector('div.ingredients-container')
